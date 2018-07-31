e3d4f5059b38e2986268114d264825d182cf85c4
Your Name <you@example.com>
2018-07-31 23:56:45 +0800

    增加亂數出題。

--- color_feeling_test/ViewController.swift
+++ color_feeling_test/ViewController.swift
@@ -23,6 +23,10 @@ class ViewController: UIViewController {
     var green: CGFloat = 0
     var blue: CGFloat = 0
     
+    var question_red: CGFloat = 0
+    var question_green: CGFloat = 0
+    var question_blue: CGFloat = 0
+    
     @IBAction func slider_ColorChange(_ sender: UISlider) {
         red = CGFloat(Int(slider_ColorChange[0].value))/255
         green = CGFloat(Int(slider_ColorChange[1].value))/255
@@ -58,5 +62,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        question_red = CGFloat(Int.random(in: 0...255))/255
+        question_green = CGFloat(Int.random(in: 0...255))/255
+        question_blue = CGFloat(Int.random(in: 0...255))/255
+        label_Question.backgroundColor = UIColor(red: question_red, green: question_green, blue: question_blue, alpha: 1)
+        print(question_red * 255)
+        print(question_green * 255)
+        print(question_blue * 255)
+    }
+    
+    @IBAction func button_Submit(_ sender: Any) {
+        if question_red == red && question_green == green && question_blue == blue {
+           print("答對了")
+        }
+    }
+    
+    @IBAction func button_NextQuestuon(_ sender: Any) {
+        question_red = CGFloat(Int.random(in: 0...255))/255
+        question_green = CGFloat(Int.random(in: 0...255))/255
+        question_blue = CGFloat(Int.random(in: 0...255))/255
+        label_Question.backgroundColor = UIColor(red: question_red, green: question_green, blue: question_blue, alpha: 1)
+    }
+    
+    @IBAction func button_AnnounceAnswer(_ sender: Any) {
+        print(question_red * 255)
+        print(question_green * 255)
+        print(question_blue * 255)
     }
 }
