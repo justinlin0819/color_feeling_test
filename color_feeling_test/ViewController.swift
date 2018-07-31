6c6a90f5e469d3d8aca174a51a40583d156f1648
Your Name <you@example.com>
2018-07-31 15:23:31 +0800

    Initial Commit

---
+++ color_feeling_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  color_feeling_test
+//
+//  Created by Justin Lin on 2018/7/31.
+//  Copyright © 2018年 Justin Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
